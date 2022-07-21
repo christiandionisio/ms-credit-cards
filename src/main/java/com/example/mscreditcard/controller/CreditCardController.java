0ed6cb1468373f98ffc2b4a976f30808bb86d5c8
Alisson <arteaga10alisson@gmail.com>
2022-07-21 17:44:38 -0500

    add logs example

--- src/main/java/com/example/mscreditcard/controller/CreditCardController.java
+++ src/main/java/com/example/mscreditcard/controller/CreditCardController.java
@@ -2,6 +2,8 @@ package com.example.mscreditcard.controller;
 
 import com.example.mscreditcard.model.CreditCard;
 import com.example.mscreditcard.service.ICreditCardService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
@@ -14,8 +16,15 @@ public class CreditCardController {
     @Autowired
     private ICreditCardService service;
 
+    private static final Logger logger = LogManager.getLogger(CreditCardController.class);
+
     @GetMapping
     public Flux<CreditCard> findAll() {
+        logger.debug("Debugging log");
+        logger.info("Info log");
+        logger.warn("Hey, This is a warning!");
+        logger.error("Oops! We have an Error. OK");
+        logger.fatal("Damn! Fatal error. Please fix me.");
         return service.findAll();
     }
 
