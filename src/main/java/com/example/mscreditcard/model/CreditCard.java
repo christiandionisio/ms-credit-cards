cc9958983cc1b7fe1ea717ffba3962f3b88e0596
Christian Dionisio <christiandionisio9432@gmail.com>
2022-07-21 17:03:37 -0500

    @Id added

--- src/main/java/com/example/mscreditcard/model/CreditCard.java
+++ src/main/java/com/example/mscreditcard/model/CreditCard.java
@@ -2,17 +2,21 @@ package com.example.mscreditcard.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.math.BigDecimal;
 
 @Document(collection = "credit_cards")
 @Data
 @AllArgsConstructor
 public class CreditCard {
+    @Id
     private String creditCardId;
     private String creditCardNumber;
     private String expirationDate;
     private String cvv;
-    private String creditLimit;
+    private BigDecimal creditLimit;
     private String category;
     private String customerId;
 }
