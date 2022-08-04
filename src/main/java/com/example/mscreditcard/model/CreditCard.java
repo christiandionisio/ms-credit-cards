26805bda7b9f9bf4f9fbdc61e4d8256e2585d2d8
alisson <arteaga10alisson@gmail.com>
2022-08-04 17:08:05 -0500

    add checkstyle

--- src/main/java/com/example/mscreditcard/model/CreditCard.java
+++ src/main/java/com/example/mscreditcard/model/CreditCard.java
@@ -7,7 +7,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 /**
- * Credit Card document
+ * Credit Card document.
  *
  * @author Alisson Arteaga / Christian Dionisio
  * @version 1.0
