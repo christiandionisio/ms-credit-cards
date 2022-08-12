71d2cfb5cce9def9fc9ec5c39958cfd3aec1b6c6
Christian Dionisio <christiandionisio9432@gmail.com>
2022-08-12 12:52:24 -0500

    overdue debt validation added in test

--- src/test/java/com/example/mscard/service/CardServiceImplTest.java
+++ src/test/java/com/example/mscard/service/CardServiceImplTest.java
@@ -1,12 +1,20 @@
 package com.example.mscard.service;
 
+import com.example.mscard.dto.CreditDto;
+import com.example.mscard.dto.CustomerDto;
+import com.example.mscard.error.CustomerHasCreditDebtException;
+import com.example.mscard.error.CustomerHasDebtException;
+import com.example.mscard.model.Card;
+import com.example.mscard.provider.CardProvider;
 import com.example.mscard.repo.CardRepository;
+import com.example.mscard.util.CardBusinessRulesUtil;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
 @SpringBootTest
@@ -14,6 +22,9 @@ class CardServiceImplTest {
 
     @MockBean
     private CardRepository repository;
+
+    @MockBean
+    private CardBusinessRulesUtil cardBusinessRulesUtil;
 
     @Autowired
     private CardServiceImpl service;
@@ -26,6 +37,67 @@ class CardServiceImplTest {
         StepVerifier.create(service.findCreditCardByCustomerIdAndHasDebt("1", true))
             .expectComplete()
             .verify();
+    }
+
+    @Test
+    void createTest() {
+
+        Mockito.when(cardBusinessRulesUtil.findCustomerById(Mockito.anyString()))
+            .thenReturn(Mono.just(getCustomerTest()));
+
+        Mockito.when(repository.save(Mockito.any(Card.class)))
+            .thenReturn(Mono.just(CardProvider.getCard()));
+
+        Mockito.when(cardBusinessRulesUtil.findCreditWithOverdueDebt(Mockito.anyString()))
+            .thenReturn(Flux.empty());
+
+        Mockito.when(repository.findCardByCustomerIdAndHasDebt(Mockito.anyString(), Mockito.anyBoolean()))
+            .thenReturn(Flux.empty());
+
+        StepVerifier.create(service.create(CardProvider.getCard()))
+            .expectNextCount(1)
+            .verifyComplete();
+    }
+
+    @Test
+    void createTestWithCustomerHasCreditDebtExceptionTest() {
+
+        Mockito.when(cardBusinessRulesUtil.findCustomerById(Mockito.anyString()))
+                .thenReturn(Mono.just(getCustomerTest()));
+
+        Mockito.when(repository.save(Mockito.any(Card.class)))
+                .thenReturn(Mono.just(CardProvider.getCard()));
+
+        Mockito.when(cardBusinessRulesUtil.findCreditWithOverdueDebt(Mockito.anyString()))
+                .thenReturn(Flux.just(new CreditDto()));
+
+        StepVerifier.create(service.create(CardProvider.getCard()))
+                .expectError(CustomerHasCreditDebtException.class)
+                .verify();
+    }
+
+    @Test
+    void createTestWithCustomerHasDebtExceptionTest() {
+
+        Mockito.when(cardBusinessRulesUtil.findCustomerById(Mockito.anyString()))
+                .thenReturn(Mono.just(getCustomerTest()));
+
+        Mockito.when(repository.save(Mockito.any(Card.class)))
+                .thenReturn(Mono.just(CardProvider.getCard()));
+
+        Mockito.when(cardBusinessRulesUtil.findCreditWithOverdueDebt(Mockito.anyString()))
+                .thenReturn(Flux.empty());
+
+        Mockito.when(repository.findCardByCustomerIdAndHasDebt(Mockito.anyString(), Mockito.anyBoolean()))
+                .thenReturn(Flux.just(CardProvider.getCard()));
+
+        StepVerifier.create(service.create(CardProvider.getCard()))
+                .expectError(CustomerHasDebtException.class)
+                .verify();
+    }
+
+    private CustomerDto getCustomerTest() {
+        return new CustomerDto();
     }
 
 }
